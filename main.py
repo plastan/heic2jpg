ba3f68a5994d54a8dbcf9c23cbdad748cf245a2d
plastan <angelofthousandeyes@gmail.com>
2023-09-17 00:17:52 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from PIL import Image
+from pillow_heif import register_heif_opener
+import os,sys
+register_heif_opener()
+try:
+    source = sys.argv[1]
+except:
+    source = os.getcwd()
+try:
+    dest = sys.argv[2]
+except:
+    dest = os.path.join(source,"result")
+files = os.listdir(source)
+heic_files = [i for i in files if 'HEIC' in i]
+os.makedirs(dest,exist_ok=True)
+for i in heic_files:
+    try:
+        TargetFile= f'{dest}/{i}'.replace("HEIC",'jpg')
+        tfile = open (TargetFile,'wb')
+        image = Image.open(source+i)
+        image.save(tfile)
+        image.close()
+    except Exception as e:
+        print("error with ",e)
+
